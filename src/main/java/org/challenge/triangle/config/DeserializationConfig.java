50f8fe4795c2230a73ae708059ea738640a25254
Adelina <adelina.alexandru93@yahoo.com>
2018-10-29 21:52:28 +0200

    disabled conversion of float to int in deserializer

---
+++ src/main/java/org/challenge/triangle/config/DeserializationConfig.java
@@ -0,0 +1,19 @@
+package org.challenge.triangle.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class DeserializationConfig {
+    @Bean
+    public MappingJackson2HttpMessageConverter jacksonConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.ACCEPT_FLOAT_AS_INT);
+        converter.setObjectMapper(mapper);
+        return converter;
+    }
+}
